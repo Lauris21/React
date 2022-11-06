fdf0e967cdcf53f97d7e3013f89c77e3b8aaa0bb
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-06 12:33:10 +0100

    Prueba Tecnica

---
+++ PruebaTecnica-React/app/src/components/notFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import './notFound.css';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="popular">Popular Titles</h1>
+      <p className="notFound">Sorry! We are working on it.</p>
+    </div>
+  );
+};
+
+export default NotFound;
--- PruebaTecnica-React/app/src/main.jsx
+++ PruebaTecnica-React/app/src/main.jsx
@@ -2,24 +2,25 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
-import Home from "./pages/home/Home";
-import Movies from "./pages/movies/Movies";
-import Series from "./pages/series/Series";
-
+import Home from './pages/home/Home';
+import Movies from './pages/movies/Movies';
+import Series from './pages/series/Series';
+import NotFound from './components/notFound/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename="/">
       <Routes>
-        <Route path='/' element={<App />} >
-        <Route index element={<Home />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/series' element={<Series />} />
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
